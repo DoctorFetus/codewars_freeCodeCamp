aef4a73449528f6986501f5c81a2df3830d0d4b4
Mikhail_Palkin <thefireofcoall@gmail.com>
2023-02-21 19:39:04 +0300

    add loops

--- practice_course/js/script.js
+++ practice_course/js/script.js
@@ -7,7 +7,23 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-const question = prompt("Один из последних просмотренных фильмов?", "");
-const answer = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[question] = answer;
+let i = 0;
+while (i != 2) {
+    const question = prompt("Один из последних просмотренных фильмов?", "");
+    if (question.length > 50 || question.length == 0) {
+        continue;
+    };
+    const answer = prompt("На сколько оцените его?", "");
+    personalMovieDB.movies[question] = answer;
+    i++;
+};
+if (numberOfFilms < 10) {
+    console.log("Просмотренно довольно мало фильмов")
+} else if (numberOfFilms <= 30) {
+    console.log("Вы классический зритель") 
+} else if (numberOfFilms > 30) {
+    console.log("Вы киноман")
+} else {
+    console.log("Произошла ошибка")
+};
 console.log(personalMovieDB)
