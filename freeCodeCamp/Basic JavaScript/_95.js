258c7241e4adb362e1d5cb73136446c2e216cd2e
Mikhail_Palkin <thefireofcoall@gmail.com>
2023-02-28 23:34:14 +0300

    end Basis JS

---
+++ freeCodeCamp/Basic JavaScript/_95.js
@@ -0,0 +1,45 @@
+// Setup
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if (value == "") {
+        delete records[id][prop];
+        return records;
+    }
+    
+    else if (prop != "tracks") {
+      records[id][prop] = value;
+      return records;
+    }
+    
+    else if (prop = "tracks") {
+      if (records[id].hasOwnProperty(prop)) {
+        records[id][prop].push(value);
+        return records;
+      } else {
+        records[id][prop] = [value]
+        return records;
+      }
+    }
+  };
+  
+  console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"));
---
+++ freeCodeCamp/_83.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Venya",
+    legs: 4,
+    tails: 1,
+    friends: ["not me"] 
+  
+    // Only change code above this line
+  };
